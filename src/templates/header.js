4d49e17fcdccc3016076d7ecb5249c467782d480
Juan Ignacio Riccio <juanignacio.riccio@gmail.com>
2022-03-31 06:41:13 -0300

    arreglos varios

--- src/templates/header.js
+++ src/templates/header.js
@@ -3,12 +3,12 @@ const Header = () => {
         <div class="header-main">
             <div class="header-logo">
                 <h1>
-                    <a href="#/">100Tifi.co</a>
+                    <a href="./">100Tifi.co</a>
                 </h1>
             </div>
 
             <div class="header-nav">
-                <a href="#/">Characters</a>
+                <a href="./">Characters</a>
                 <a href="#/episodes">Episodes</a>
                 <a href="#/locations">Locations</a>
             </div>
